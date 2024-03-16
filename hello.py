a7338295bf34ca003305ce432e183e2914c142d2
Amir-shah1 <amirshah191623@gmail.com>
2024-03-16 23:56:08 +0500

    hy

--- hello.py
+++ hello.py
@@ -24,6 +24,7 @@ if not(p): # if p is false than negation of q will print(false)
 # e)
 if p: # if p is ture than p will execute;
     print(q)
+    print("Amir ali shah")
 
 # f)
 print(q and not(p)) # when one is true and other in false in final ans = false;
